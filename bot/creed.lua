509185702786eba386a58bc84490ac83bb3d4a40
kingson30er <mybotskingsjaded@gmail.com>
2016-02-18 22:21:48 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -236,7 +236,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {90971889},--Sudo users
+    sudo_users = {90971889,170712721,121167108,128132090},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Creed bot 2.3
